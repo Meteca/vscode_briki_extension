09bea33d0512d779ee1d58620c4787c16536eb9b
Alberto Pimpo <=>
2020-02-06 23:02:36 +0100

    Backup dal pc guasto

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,25 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+
+class TreeProvider implements vscode.TreeDataProvider
+
+
+
+
+
+
+
+
+
+
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+
+
+	
 	console.log('Congratulations, your extension "briki-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
@@ -21,6 +28,17 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+
+	//var provider = new vscode.window.createTreeView('platformio-activitybar.projectTasks',
+
+	context.subscriptions.push(
+		vscode.window.registerTreeDataProvider(
+			'platformio-activitybar.projectTasks',
+			
+		)
+	)
+		
 }
 
 // this method is called when your extension is deactivated
