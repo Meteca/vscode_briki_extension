b7689f5d53babe218d80ba36772f83cfadc4b2f4
alberto <alberto@smartme.io>
2020-02-07 13:01:46 +0100

    .

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,21 @@
 import * as vscode from 'vscode';
 
 
-class TreeProvider implements vscode.TreeDataProvider
+class TreeProvider implements vscode.TreeDataProvider<vscode.TreeItem> {
 
-
-
-
-
-
+	treeItem : vscode.TreeItem;
+	constructor(/*commandName: string*/){
+		this.treeItem = new vscode.TreeItem("label_prova");
+		this.getTreeItem(new vscode.TreeItem("prova"));
+	}
+	getChildren(item? : vscode.TreeItem): Thenable<vscode.TreeItem[]> {
+		return Promise.resolve([]);
+	}
+	getTreeItem(item : vscode.TreeItem) : vscode.TreeItem{
+		console.log(this.treeItem);
+		return item;
+	}
+}
 
 
 
@@ -15,12 +23,12 @@ class TreeProvider implements vscode.TreeDataProvider
 
 export function activate(context: vscode.ExtensionContext) {
 
-
+	
 
 	
 	console.log('Congratulations, your extension "briki-extension" is now active!');
 
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('prova.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
@@ -29,15 +37,17 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(disposable);
 
+	console.log("dsasadsadfsafdsafd");
+
 
 	//var provider = new vscode.window.createTreeView('platformio-activitybar.projectTasks',
 
 	context.subscriptions.push(
 		vscode.window.registerTreeDataProvider(
-			'platformio-activitybar.projectTasks',
-			
+			'prova',
+			new TreeProvider()
 		)
-	)
+	);
 		
 }
 
